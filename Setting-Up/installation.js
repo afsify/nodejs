c89f10f130ed7f428c2980687aa5c53d1f05665e
AFSAL333 <afsalafs8008@gmail.com>
2024-07-12 21:39:36 +0530

    installation added

---
+++ Setting-Up/installation.js
@@ -0,0 +1,22 @@
+//! =================================== Setting Up Node.js Environment ===================================
+
+// Installation:
+
+// For macOS and Linux
+//* brew install node
+
+// For Windows
+//* choco install nodejs
+
+// Node.js REPL:
+
+//* $ node
+//* > console.log('Hello, REPL!');
+
+// Node Version Manager (NVM):
+
+// # Install NVM
+//* curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+// # Use NVM to install Node.js
+//* nvm install node
